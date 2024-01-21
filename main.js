4f86009ddc95ad2653e91dff9b546ad5fb2b1f99
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 18:37:11 -0500

    Add test code to create an example grid in console

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
 // create a 3 x 3 grid in console.
-function Gameboard() {
+function Gameboard(rowCount, columnCount) {
    const rows = 3;
    const columns = 3;
    const gameboard = [];
+   const grid = Array.from(new Array(5),()=>Array.from(new Array(5),()=>"-"));
+
+const rotate = grid => 
+  grid[0].map(
+    (_,y)=>grid.map(
+      (_,x)=>[y,x]
+    )
+  ).map(
+    row=>row.map(([x,y])=>grid[y][x])
+  );
+const format = grid => grid.map(x=>x.join(" ")).join("\n");
+//set some values of grid
+[[0,2],[1,2],[2,2],[3,2],[4,2]].forEach(
+  ([x,y])=>grid[x][y]="X"
+);
+
+//you can map the grid to columns first, it'll look like it's rotated
+//  unless you generate the columns in div float lefts
+console.log("map grid columns first:")
+console.log(format(grid));
+
+//you can rotate the grid to build each row and then each column like html table
+console.log("map grid rows first:")
+console.log(format(rotate(grid)));
+}
+
+window.onload = function() {
+   Gameboard();
 }
 
 // create player1 and player 2.
 const playerOne = 'X';
 const playerTwo = 'O';
 let currentPlayer = playerOne;
+
+
 // alternate player turns.
 
 // let each player select a spot for their marker each turn.
