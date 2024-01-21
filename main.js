a1a07975f81d694f03dabbb0db9321fd033425ce
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 16:13:50 -0500

    Add psuedocode (lines 9-17)

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function Gameboard() {
 }
 
 console.log(Gameboard);
+
+// create a 3 x 3 grid in console.
+// create player1 and player 2.
+// alternate player turns.
+// let each player select a spot for their marker each turn
+// when a player choses a spot, push X/O and
+// replace the grid coords with X/O.
+// create function to check for winner 
+// horizontally, vertically, & (anti) diagonally.
+// when someone wins, clear gameboard
