f85bae79e29a0914b3a436d0134602683424be0a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 18:42:02 -0500

    ESlint for indentation errors

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 // create a 3 x 3 grid in console.
 function Gameboard(rowCount, columnCount) {
-   const rows = 3;
-   const columns = 3;
-   const gameboard = [];
-   const grid = Array.from(new Array(3),()=>Array.from(new Array(3),()=>"-"));
+  const rows = 3;
+  const columns = 3;
+  const gameboard = [];
+  const grid = Array.from(new Array(3),()=>Array.from(new Array(3),()=>"-"));
 
-const rotate = grid => 
-  grid[0].map(
-    (_,y)=>grid.map(
-      (_,x)=>[y,x]
-    )
-  ).map(
-    row=>row.map(([x,y])=>grid[y][x])
+  const rotate = grid => 
+    grid[0].map(
+      (_,y)=>grid.map(
+        (_,x)=>[y,x]
+      )
+    ).map(
+      row=>row.map(([x,y])=>grid[y][x])
+    );
+  const format = grid => grid.map(x=>x.join(" ")).join("\n");
+  //set some values of grid
+  [[0,2],[1,2],[2,2]].forEach(
+    ([x,y])=>grid[x][y]="X"
   );
-const format = grid => grid.map(x=>x.join(" ")).join("\n");
-//set some values of grid
-[[0,2],[1,2],[2,2]].forEach(
-  ([x,y])=>grid[x][y]="X"
-);
 
-//you can map the grid to columns first, it'll look like it's rotated
-//  unless you generate the columns in div float lefts
-console.log("map grid columns first:")
-console.log(format(grid));
+  //you can map the grid to columns first, it'll look like it's rotated
+  //  unless you generate the columns in div float lefts
+  console.log("map grid columns first:")
+  console.log(format(grid));
 
-//you can rotate the grid to build each row and then each column like html table
-console.log("map grid rows first:")
-console.log(format(rotate(grid)));
+  //you can rotate the grid to build each row and then each column like html table
+  console.log("map grid rows first:")
+  console.log(format(rotate(grid)));
 }
 
 window.onload = function() {
-   Gameboard();
+  Gameboard();
 }
 
 // create player1 and player 2.
