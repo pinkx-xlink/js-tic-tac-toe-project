a4357698f88b9a7fcd245be9d66c7418277cd029
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 18:28:17 -0500

    Create player variables

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+// create a 3 x 3 grid in console.
 function Gameboard() {
    const rows = 3;
    const columns = 3;
    const gameboard = [];
 }
 
-console.log(Gameboard);
-
-// create a 3 x 3 grid in console.
 // create player1 and player 2.
+const playerOne = 'X';
+const playerTwo = 'O';
+let currentPlayer = playerOne;
 // alternate player turns.
-// let each player select a spot for their marker each turn
+
+// let each player select a spot for their marker each turn.
+
 // when a player choses a spot, push X/O and
 // replace the grid coords with X/O.
+
 // create function to check for winner 
 // horizontally, vertically, & (anti) diagonally.
-// when someone wins, clear gameboard
+
+// when someone wins, gameover & clear gameboard
