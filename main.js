700fa39f944476cfcf9c2fd49a5eb8f12159be96
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-23 12:50:38 -0500

    Troubleshoot adding a marker to the gameboard, and ESlint for indentation problems

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-
-
 // create a 3 x 3 grid in console.
 function Gameboard () {
   const rows = 3;
   const columns = 3;
   let marker;
   const gameboard = Array.from(new Array(3),()=>Array.from(new Array(3),()=>"|"));
-   
   const rotate = gameboard => 
     gameboard[0].map(
       (_,y)=>gameboard.map(
@@ -19,28 +16,26 @@ function Gameboard () {
   
   console.log(format(gameboard));
 }
-
 window.onload = function () {
   Gameboard();
   setPlayer();
 }
-
 // create player1 and player 2.
-const playerOne = 'X';
-const playerTwo = 'O';
+const playerOne = 'Player 1';
+const playerTwo = 'Player 2';
 let currentPlayer = playerOne;
 
 // alternate player turns.
 function setPlayer() {
-   if (currentPlayer == playerOne) {
-      currentPlayer = playerTwo;
-      currentMarker = playerTwoMarker
-      console.log('Player Twos turn. Marker: O');
-   } else if (currentPlayer == playerTwo) {
-      currentPlayer = playerOne;
-      currentMarker = playerOneMarker;
-      console.log(`Player One's turn. Marker: X`);
-   }
+  if (currentPlayer == playerOne) {
+    currentPlayer = playerTwo;
+    currentMarker = playerTwoMarker
+    console.log('Player Twos turn. Marker: O');
+  } else if (currentPlayer == playerTwo) {
+    currentPlayer = playerOne;
+    currentMarker = playerOneMarker;
+    console.log(`Player One's turn. Marker: X`);
+  }
 }
 
 //let player place a marker 
@@ -48,10 +43,9 @@ const playerOneMarker = 'X';
 const playerTwoMarker = 'O';
 let currentMarker = 'X';
 function setMarker(x, y) {
-   const updateGameboard = Array.from(new Array(3),()=>Array.from(new Array(3),()=>"|"));
-   // setCoords code
-   setTile(x, y);
-   
+  const updateGameboard = Array.from(new Array(3),()=>Array.from(new Array(3),()=>"|"));
+  // setCoords code
+  setTile(x, y);
   const rotate = updateGameboard => 
     updateGameboard[0].map(
       (_,y)=>updateGameboard.map(
@@ -60,31 +54,28 @@ function setMarker(x, y) {
     ).map(
       row=>row.map(([x,y])=>updateGameboard[y][x])
     );
-    
   const format = updateGameboard => updateGameboard.map(x=>x.join(" ")).join("\n");
   //set some values of grid
-   // [x, y].forEach(
-   //   (x, y)=>updateGameboard[x][y]=currentMarker
-   // );
-   setPlayer();
+  // [x, y].forEach(
+  //   (x, y)=>updateGameboard[x][y]=currentMarker
+  // );
+  setPlayer();
   console.log(format(updateGameboard));
 }
-// call in the console! for ex:
-// setMarker(0, 0)
+// call in the console with setMarker(0, 0)
 
 const setTile = function(x, y) {
-   let tile;
-   tile = x.toString() + "-" + y.toString();
-   console.log(`coords: ${tile}`);
-   function setCoordinates() {
-      let coords = tile.split("-");
-      x = parseInt(coords[0]);
-      y = parseFloat(coords[1]);
-      console.log(coords);
-   }
-   setCoordinates();
+  let tile;
+  tile = x.toString() + "-" + y.toString();
+  console.log(`coords: ${tile}`);
+  function setCoordinates(tile) {
+    let coords = tile.split("-");
+    x = parseInt(coords[0]);
+    y = parseFloat(coords[1]);
+    console.log(`${currentPlayer} placed an ${currentMarker} in coord: ${coords}`);
+  }
+  setCoordinates(tile);
 };
-
 
 // when a player choses a spot, push X/O and
 // replace the grid coords with X/O.
