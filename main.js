1759d0eefe3f6d1a45efdce9bd0db8f20e482160
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-29 10:07:47 -0500

    Move the new HTML code out of the Gameboard function

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
+function populateGameboard() {
+  htmlTiles = 0;
+  for(let i = 0; i < 8; i++){
+     if (i < 9) {
+        htmlGameboard.innerHTML += `<button id ="my-tiles" onclick="placeMyMarker()"></div>`;
+        console.log('counting tiles...');
+     } else {
+        console.log('done');
+        return;
+     }
+  }
+}
 function placeMyMarker() {
    let currentTile = document.querySelector('button');
    currentTile.innerHTML = `<p>beep</p>`;
    console.log('you clicked a tile.');
 };
-// create a 3 x 3 grid in console.
-function Gameboard () {
-
-   const htmlGameboard = document.querySelector('#my-gameboard');
+const htmlGameboard = document.querySelector('#my-gameboard');
    let htmlTiles = document.createElement('button');
    htmlTiles.classList.add('button');
    htmlTiles.setAttribute('id', 'my-tiles');
    htmlGameboard.appendChild(htmlTiles);
    
- 
-   function populateGameboard() {
-      htmlTiles = 0;
-      for(let i = 0; i < 8; i++){
-         if (i < 9) {
-            htmlGameboard.innerHTML += `<button id ="my-tiles", onclick="placeMyMarker()"></div>`;
-            console.log('counting tiles...');
-         } else {
-            console.log('done');
-            return;
-         }
-      }
-   }
+// create a 3 x 3 grid in console.
+function Gameboard () { 
+   
    populateGameboard();
   const rows = 3;
   const columns = 3;
