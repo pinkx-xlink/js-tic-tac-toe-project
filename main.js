ad7ee4e02d33eaafdda60953c96b303deed298c4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-02-18 20:59:50 -0500

    Delete comments from lines 1-5, 7-11, and 16-17

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-// change the grid to an array, 
-// each quare being one object,
-// similar to my library book project.
-// then allow each array element to be accessed and
-// add a marker (x or o) to it.
 function placeMyMarker() {
-    // this line only changes inner HTML of the first el
-  //  let currentTile = document.querySelectorAll('#my-tiles');
-  //  currentTile.classList.add('button');
-  //  currentTile.setAttribute('id', 'current-tile');
-  //  htmlTiles.appendChild(currentTile);
    const currentTile = this.tiles;
    console.log('you clicked a tile.');
 }
 const htmlGameboard = document.querySelector('#my-gameboard');
-// /htmlGameboard.addEventListener('click', placeMyMarker);
-// htmlTiles.addEventListener('click', placeMyMarker);
-
 const cell = document.querySelectorAll(".cell");
