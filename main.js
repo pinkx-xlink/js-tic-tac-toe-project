45cba7886a860fde26cc5dd2ced352d251afdac7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-02-18 21:03:48 -0500

    Add a forEach loop to cell that runs placeMyMarker on click

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function placeMyMarker() {
-   console.log('you clicked a tile.');
-}
 const htmlGameboard = document.querySelector('#my-gameboard');
-const cell = document.querySelectorAll(".cell");
+let cell = document.querySelectorAll(".cell");
+
+cell.forEach(addEventListener("click", placeMyMarker));
+function placeMyMarker(cell) {
+  console.log('you clicked a tile.');
+  this.innerHTML = 'x';
+}
